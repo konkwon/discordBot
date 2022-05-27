44dd98cf7c4e40c295616a1ba828d58abb81cdce
Koo Hyuk Kwon <kwonk1@unlv.nevada.edu>
2022-05-27 04:32:23 -0700

    test message responds

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # discordBot
-discord bot pulling data from league of legends website / wiki using beautiful soup
-
-Working on fixing the bot and potentially implementing Riot API to feature more information
+noncommercial discord bot pulling data from ffxiv wiki / saltedxiv using beautiful soup
---
+++ ffBot.py
@@ -0,0 +1,28 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready(): 
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('a'):
+        channel = message.channel
+        await channel.send('Say hello!')
+
+        def check(m):
+            return m.content == 'hello' and m.channel == channel
+
+        msg = await client.wait_for('message', check=check)
+        await channel.send('Hello {.author}!'.format(msg))
+
+
+
+client.run(TOKEN)
