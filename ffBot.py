557fb782f08c3813fa5b73ff8f5ea043c8e49c75
Koo Hyuk Kwon <kwonk1@unlv.nevada.edu>
2022-05-31 16:59:03 -0700

    added XIVAPI token

--- ffBot.py
+++ ffBot.py
@@ -1,10 +1,15 @@
 import os
 import discord
+import asyncio
+import aiohttp
+import pyxivapi
 import supportedConents
+
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+DISC_TOKEN = os.getenv('DISCORD_TOKEN')
+XIV_TOKEN = os.getenv('XIVAPI_TOKEN')
 
 client = discord.Client()
 
@@ -14,21 +19,31 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.bot: # no infinite loops from bot message
+    if message.author.bot: # no infinite loops from bot response message
         return
 
     if message.content.startswith("!help"):
         channel = message.channel
-        await channel.send("List of commands you can use: \n !raid \n !character")
+        await channel.send("List of commands you can use: \n!raid [encounter name e.g. zodiark]\n!character [First name Last name]")
 
     if message.content.startswith("!raid"):
-        reply = str(message.content)[6:]
+        encounter = str(message.content)[6:]
         channel = message.channel
-        if not reply: # no raid specified / empty after !raid
-            await channel.send("Type raid name after !raid. e.g. !raid p4s")
+        if not encounter: # no raid specified / empty after !raid
+            await channel.send("Type raid name after !raid.\ne.g. !raid p4s")
         else:
-            await channel.send(supportedConents.encounters[reply])
+            await channel.send(supportedConents.encounters[encounter])
+    
+    if message.content.startswith("!char"):
+        name = str(message.content)[6:]
+        channel = message.channel
+        if not name: # no raid specified / empty after !raid
+            await channel.send("Type character name and server name after !char.\ne.g. !char Ravi Lavi Leviathan")
+        else:
+            client = pyxivapi.XIVAPIClient(api_key=XIV_TOKEN)
+
+            
 
 
 
-client.run(TOKEN)
+client.run(DISC_TOKEN)
